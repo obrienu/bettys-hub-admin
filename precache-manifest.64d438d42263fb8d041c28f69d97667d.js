18487686f96270c8a4c5a70d6f921a4788917827
unknown <obrienules@gmail.com>
2020-02-14 14:44:12 +0100

    Updates

---
+++ precache-manifest.64d438d42263fb8d041c28f69d97667d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84796ab8c608179204eda2ad20cc05ac",
+    "url": "/bettys-hub-admin/index.html"
+  },
+  {
+    "revision": "07e2ebffb63160c18759",
+    "url": "/bettys-hub-admin/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "ff2550a908f1b03c3e49",
+    "url": "/bettys-hub-admin/static/css/main.f7686863.chunk.css"
+  },
+  {
+    "revision": "07e2ebffb63160c18759",
+    "url": "/bettys-hub-admin/static/js/2.fafedefb.chunk.js"
+  },
+  {
+    "revision": "1c7f0ad687308896ffe8c0ba36060daa",
+    "url": "/bettys-hub-admin/static/js/2.fafedefb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff2550a908f1b03c3e49",
+    "url": "/bettys-hub-admin/static/js/main.e793f98c.chunk.js"
+  },
+  {
+    "revision": "32791e1af3921ca7c5db",
+    "url": "/bettys-hub-admin/static/js/runtime-main.bbf8d310.js"
+  }
+]);
